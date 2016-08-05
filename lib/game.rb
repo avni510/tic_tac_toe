56e6782c9e737847683d4febb5945c83b2f1a5fb
avni510 <avni510@gmail.com>
2016-08-05 11:53:55 -0500

    Game Class
    Finished writing and testing the Game class run method. The executable
    needs to be updated.

--- lib/game.rb
+++ lib/game.rb
@@ -2,14 +2,19 @@ module TicTacToe
 
   class Game 
     
-    def initialize(game_loop, symbol_loop, end_game, interface)
+    def initialize(game_loop, symbol_loop, end_game)
       @game_loop = game_loop
       @symbol_loop = symbol_loop
       @end_game = end_game
+      @tied_or_player_won = nil
     end
 
     def run 
       @symbol_loop.symbol_selection
+      until @tied_or_player_won 
+        @tied_or_player_won = @game_loop.instructions(@tied_or_player_won, @symbol_loop.player1_symbol, @symbol_loop.player2_symbol)
+      end
+      @end_game.finished_game(@tied_or_player_won)
     end
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -6,12 +6,22 @@ module TicTacToe
     let(:mock_game_loop) { double () }
     let(:mock_symbol_loop) { double () } 
     let(:mock_end_game) { double () } 
-    let(:mock_interface) { double () } 
-    let(:game) { Game.new(mock_game_loop, mock_symbol_loop, mock_end_game, mock_interface) }
+    let(:game) { Game.new(mock_game_loop, mock_symbol_loop, mock_end_game) }
 
     describe ".run" do 
       it "can run through an entire tic tac toe game" do 
         expect(mock_symbol_loop).to receive(:symbol_selection)
+
+        expect(mock_symbol_loop).to receive(:player1_symbol).and_return("O")
+        expect(mock_symbol_loop).to receive(:player2_symbol).and_return("X")
+        expect(mock_game_loop).to receive(:instructions).with(nil, "O", "X").and_return(nil)
+
+        expect(mock_symbol_loop).to receive(:player1_symbol).and_return("O")
+        expect(mock_symbol_loop).to receive(:player2_symbol).and_return("X")
+        expect(mock_game_loop).to receive(:instructions).with(nil, "O", "X").and_return("2")
+
+        expect(mock_end_game).to receive(:finished_game).with("2")
+
         game.run
       end
     end
