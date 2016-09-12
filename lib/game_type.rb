37017cfd695ba90f414d340de7e4cea85b7c8e12
avni510 <avni510@gmail.com>
2016-09-12 10:51:35 -0500

    Refactored game_type
    * reduced duplication
    * #game_type_assignment does not have any loose strings

--- lib/game_type.rb
+++ lib/game_type.rb
@@ -15,25 +15,24 @@ module TicTacToe
       }
       @console_ui.display_game_menu(menu_choices)
       user_game_choice = valid_game_menu_choice(menu_choices)
-      game_type_assignment(user_game_choice)
+      game_type_assignment(user_game_choice, menu_choices)
     end
 
     private
 
     def valid_game_menu_choice(menu_choices)
       user_game_choice = @console_ui.user_input
-      menu_selection_options = menu_choices.keys
-      until menu_selection_options.include?(user_game_choice)
+      until menu_choices.has_key?(user_game_choice)
         @console_ui.valid_menu_choice
         user_game_choice = @console_ui.user_input
       end
       user_game_choice
     end
 
-    def game_type_assignment(user_game_choice)
-      if user_game_choice == '1'
+    def game_type_assignment(user_game_choice, menu_choices)
+      if user_game_choice == menu_choices.key('1. Human v. Human')
         @human_v_human = true
-      elsif user_game_choice == '2'
+      elsif user_game_choice == menu_choices.key('2. Human v. Simple Computer')
         @human_v_simp_comp = true
       end
     end
