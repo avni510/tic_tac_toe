09515def46f6b8ad4e79f85cff5c248748a67191
avni510 <avni510@gmail.com>
2016-09-13 11:15:32 -0500

    Human initialization is extracted out into a private method

--- lib/player_setup.rb
+++ lib/player_setup.rb
@@ -5,12 +5,18 @@ module TicTacToe
 
     def player_assignment(marker1, marker2, console_ui, game_type)
       if game_type.human_v_human
-        @p1 = Human.new(marker1, console_ui)
-        @p2 = Human.new(marker2, console_ui)
+        @p1 = human_player(marker1, console_ui) 
+        @p2 = human_player(marker2, console_ui)
       elsif game_type.human_v_simp_comp
-        @p1 = Human.new(marker1, console_ui)
+        @p1 = human_player(marker1, console_ui)
         @p2 = Computer.new(marker2, console_ui)
       end
+    end
+
+    private 
+
+    def human_player(marker, console_ui)
+      Human.new(marker, console_ui)
     end
   end
 end
